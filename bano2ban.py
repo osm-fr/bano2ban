89ef58d4f8d645b76d078c85ac9d2f1b4c8e6613
cquest <cquest@openstreetmap.fr>
2017-12-03 12:26:47 +0100

    appels API via fonction unique 'call_api'

--- bano2ban.py
+++ bano2ban.py
@@ -20,10 +20,26 @@ def getAuthToken():
         'client_secret': secret.secret,
         'email': 'bano@openstreetmap.fr'
         }
-    auth = requests.post(api+'/token/', json=payload)
-    token = json.loads(auth.text)
+    (status,token) = call_api('POST','/token/', payload)
     return token['access_token']
 
+def call_api(method, endpoint, payload=None):
+    # header si on est authentifié
+    if auth_token is not None:
+        headers={'Authorization':'Bearer '+auth_token}
+    else:
+        headers={}
+    if method == 'GET':
+        result = requests.get(api+endpoint, headers=headers)
+    elif method == 'POST':
+        result = requests.post(api+endpoint, headers=headers, json=payload)
+    elif method == 'PATCH':
+        result = requests.patch(api+endpoint, headers=headers, json=payload)
+    elif method == 'DELETE':
+        result = requests.delete(api+endpoint, headers=headers, json=payload)
+    if (result.status_code != 200):
+        print(result.text)
+    return(result.status_code, json.loads(result.text))
 
 # on récupère le token d'authentification pour les appels suivants
 auth_token = getAuthToken()
@@ -36,24 +52,22 @@ cur.execute("SELECT left(fantoir,9) as fantoir, voie_cadastre FROM cumul_adresse
 groups = cur.fetchall()
 print(len(groups),' groupes dans BANO')
 for group in groups:
-    get = requests.get(api+'/group/fantoir:'+group['fantoir'], headers={'Authorization':'Bearer '+auth_token})
-    if get.status_code == 200: # on a trouvé la voie
-        ban_group = json.loads(get.text)
+    (status, ban_group) = call_api('GET', '/group/fantoir:'+group['fantoir'])
+    if status == 200: # on a trouvé la voie
         name = re.sub('  ',' ',group[1]) # nettoyage des espaces multiples...
         print(group['fantoir'], name)
         if name != ban_group['name']: # nom différent
             print('  Nom différent: %s <-> %s' % (name, ban_group['name']))
             if ban_group['name'] ==  ban_group['name'].upper() or len(name) != len(ban_group['name']):
-                patch = requests.patch(api+'/group/fantoir:'+group['fantoir'], headers={'Authorization':'Bearer '+auth_token}, json={'name': name, 'version': ban_group['version']+1})
-                print('  Mise à jour', patch.status_code)
+                status = call_api('PATCH', '/group/fantoir:'+group['fantoir'], payload={'name': name, 'version': ban_group['version']+1})
+                print('  Mise à jour', status)
 
         # contrôle des housenumbers de la voie
         cur2.execute("SELECT numero, st_x(geometrie) as lon, st_y(geometrie) as lat, source FROM cumul_adresses WHERE insee_com=%s AND source ~ '^OD-' and fantoir~%s order by 1;", (insee, '^'+group['fantoir']))
         housenumbers = cur2.fetchall()
 
         # récupération depuis la BAN
-        get = requests.get(api+'/housenumber?limit=1000&group=fantoir:'+group['fantoir'], headers={'Authorization':'Bearer '+auth_token})
-        ban_housenumbers = json.loads(get.text)
+        (status,ban_housenumbers) = call_api('GET','/housenumber?limit=1000&group=fantoir:'+group['fantoir'])
 
         for housenumber in housenumbers:
             od_number = re.sub('[^0-9]', '', housenumber['numero'])
@@ -75,16 +89,14 @@ for group in groups:
                         # numéro identique mais ordinal abrégé ?
                         print("    "+h['number']+h['ordinal']+" > "+housenumber['numero'])
                         payload = {'ordinal': od_ordinal, 'version': h['version']+1}
-                        patch = requests.patch(api+'/housenumber/'+h['id'], headers={'Authorization':'Bearer '+auth_token}, json=payload)
-                        print('    mise à jour housenumber/ordinal', patch.status_code)
-                        ban_num = json.loads(patch.text)
+                        (status, ban_num) = call_api('PATCH','/housenumber/'+h['id'], payload)
+                        print('    mise à jour housenumber/ordinal', status)
                         break
             if ban_num is None: # numéro non trouvé
                 print("    N° " + housenumber['numero'] + " non trouvé")
                 # on ajoute le housenumber manquant
                 payload = {'number': od_number, 'ordinal': od_ordinal, 'parent': ban_group['id'], 'attributes': {'created_from': 'Opendata Montpellier'} }
-                post = requests.post(api+'/housenumber', headers={'Authorization':'Bearer '+auth_token}, json=payload)
-                ban_num = json.loads(post.text)
+                (status, ban_num) = call_api('POST','/housenumber', payload)
                 print('    ajout housenumber > '+ban_num['id'])
                 payload = {
                     'housenumber': ban_num['id'],
@@ -93,13 +105,11 @@ for group in groups:
                     'positioning':'gps',
                     'center':{'coordinates': [housenumber['lon'],housenumber['lat']]}
                 }
-                post = requests.post(api+'/position', headers={'Authorization':'Bearer '+auth_token}, json=payload)
-                ban_pos = json.loads(post.text)
+                (status, ban_pos) = call_api('POST', '/position', payload)
                 print('      ajout position N° %s > %s' % (housenumber['numero'], ban_pos['id']))
             else:
                 # maintenant les positions...
-                get = requests.get(api+'/position?housenumber='+ban_num['id'], headers={'Authorization':'Bearer '+auth_token})
-                ban_positions = json.loads(get.text)
+                (status,ban_positions) = call_api('GET','/position?housenumber='+ban_num['id'])
                 ban_pos = None
                 for pos in ban_positions['collection']:
                     if pos['center']['coordinates'][0] == housenumber['lon'] and pos['center']['coordinates'][1] == housenumber['lat']:
@@ -113,8 +123,7 @@ for group in groups:
                         'positioning':'gps',
                         'center':{'coordinates': [housenumber['lon'],housenumber['lat']]}
                     }
-                    post = requests.post(api+'/position', headers={'Authorization':'Bearer '+auth_token}, json=payload)
-                    ban_pos = json.loads(post.text)
+                    (status, ban_pos) = call_api('POST','/position', payload)
                     print('      ajout position N° %s > %s' % (housenumber['numero'], ban_pos['id']))
 
     else:
